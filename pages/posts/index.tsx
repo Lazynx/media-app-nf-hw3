0b2ab552de2fa43eaa0747405411765d1c910ec8
Lazynx <frichaplay@gmail.com>
2024-06-09 23:44:19 +0500

    hotfix

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <section className="w-full py-8 md:py-18 lg:py-26 bg-white dark:bg-black">
                 <div className="container space-y-12 px-4 md:px-6">
                     {loading ? (
-                        <div className="inset-0 flex items-center justify-center bg-white w-full h-screen dark:bg-black">
+                        <div className="inset-0 flex items-center justify-center bg-white w-full h-screen">
                             <div className="animate-spin rounded-full border-4 border-black border-t-transparent h-14 w-14" />
                         </div>
                     ) : error ? (
